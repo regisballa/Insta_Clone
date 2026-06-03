9b23889a27c38fc8954855b538066de6c239313b
regisballa <ballaregis2008@gmail.com>
2026-06-03 10:26:53 +0200

    Create login page structure

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    let { form } = $props();
+</script>
+
+<div class="min-h-screen bg-zinc-950 flex items-center justify-center px-4">
+    <div class="w-full max-w-sm">
+
+    </div>
+</div>
