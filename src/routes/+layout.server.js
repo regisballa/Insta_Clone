fd2a81e6e36ef0de0cb78899f555bada6707616b
Regis Balla <ballaregis2008@gmail.com>
2026-06-09 23:24:09 +0200

    implement user session validation

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,4 +1,8 @@
 import { validateSession } from '$lib/server/auth.js';
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ cookies }) {
+const sessionId = cookies.get('session');
+// Gibt null zurück wenn nicht eingeloggt oder Session abgelaufen
+const user = await validateSession(sessionId);
+return { user };
 } 
